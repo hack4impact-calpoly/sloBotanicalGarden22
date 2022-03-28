b287504c28a220975e177140f2d6d9072da28711
angelakim <akim173@calpoly.edu>
2022-03-27 18:38:19 -0700

    after pull

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,40 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f67818d8cde892b703c1043922b9a4cd53f56a22
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
-import reportWebVitals from "./reportWebVitals";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
->>>>>>> 074345cb63d8ed064c396765c329f2ff8fe50549
-=======
 import "semantic-ui-css/semantic.min.css";
->>>>>>> f67818d8cde892b703c1043922b9a4cd53f56a22
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
-<<<<<<< HEAD
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
-  document.getElementById('root')
-);
->>>>>>> 074345cb63d8ed064c396765c329f2ff8fe50549
-=======
-  document.getElementById("root")
-);
->>>>>>> f67818d8cde892b703c1043922b9a4cd53f56a22
